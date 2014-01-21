367f04fbf87bc7bb97852f89dfe167442253a0a0
Matt <matthew.kuzyk@gmail.com>
2014-01-20 22:58:39 -0500

    Tabs rrrgggh

--- app/controllers/tumblr_backend_controller.rb
+++ app/controllers/tumblr_backend_controller.rb
@@ -1,39 +1,39 @@
 class TumblrBackendController < ApplicationController
-	require 'oauth/consumer'
-	@@globalSession = true
-	def tags
-		if @@globalSession
-			@@consumer=OAuth::Consumer.new "HkKQKdz8qmb2WTWTPWjd0FkRFQIGihbBkASyVdTtkIeihRFt4Z", "NScqGFcG2ymequqpFWMe7qKIs3ux0XyE6iS0ESByK6UySbMZwi", {site: "http://www.tumblr.com"}
-			@@request_token = @@consumer.get_request_token( { :oauth_callback => 'http://localhost:3000/callback' } )
-	    session[:request_token]=@@request_token
-	    redirect_to @@request_token.authorize_url
-	  end
-		# @request_token = @consumer.get_request_token(exclude_callback: true)
-		# redirect_to @request_token.authorize_url
-		# @access_token = @request_token.get_access_token
-		# @response = @access_token.get "/agreements.xml"
-		@@tags = params[:data]
-		@@tags = JSON.parse @@tags
-		prng = Random.new
-		@@tag = @@tags[prng.rand(@@tags.count)]
-		@@tag.squish!
-		@@tag.sub! " ", "+"
-		if !@@globalSession
-			note_count = 0
+  require 'oauth/consumer'
+  @@globalSession = true
+  def tags
+    if @@globalSession
+      @@consumer=OAuth::Consumer.new "HkKQKdz8qmb2WTWTPWjd0FkRFQIGihbBkASyVdTtkIeihRFt4Z", "NScqGFcG2ymequqpFWMe7qKIs3ux0XyE6iS0ESByK6UySbMZwi", {site: "http://www.tumblr.com"}
+      @@request_token = @@consumer.get_request_token( { :oauth_callback => 'http://localhost:3000/callback' } )
+      session[:request_token]=@@request_token
+      redirect_to @@request_token.authorize_url
+    end
+    # @request_token = @consumer.get_request_token(exclude_callback: true)
+    # redirect_to @request_token.authorize_url
+    # @access_token = @request_token.get_access_token
+    # @response = @access_token.get "/agreements.xml"
+    @@tags = params[:data]
+    @@tags = JSON.parse @@tags
+    prng = Random.new
+    @@tag = @@tags[prng.rand(@@tags.count)]
+    @@tag.squish!
+    @@tag.sub! " ", "+"
+    if !@@globalSession
+      note_count = 0
       @@client.tagged(@@tag, :limit => 20).each do |p|
-				if p["note_count"] > note_count
-					note_count = p["note_count"]
-					@post = p
-				end
-			end
-			@@client.reblog @@client.info['user']['name']+".tumblr.com", id: @post['id'], reblog_key: @post['reblog_key']
-			render nothing: true
-		end
-		
+        if p["note_count"] > note_count
+          note_count = p["note_count"]
+          @post = p
+        end
+      end
+      @@client.reblog @@client.info['user']['name']+".tumblr.com", id: @post['id'], reblog_key: @post['reblog_key']
+      render nothing: true
+    end
+    
 
-	end
+  end
 
-	def callback
+  def callback
     request_token = ActiveSupport::JSON.decode(@@request_token.to_json)
 
         if !(request_token.present?)
@@ -53,24 +53,24 @@ class TumblrBackendController < ApplicationController
         puts oauth_token_secret
         puts "****************************"
         @@client = Tumblr::Client.new({
-        	consumer_key: "HkKQKdz8qmb2WTWTPWjd0FkRFQIGihbBkASyVdTtkIeihRFt4Z",
-      		consumer_secret: "NScqGFcG2ymequqpFWMe7qKIs3ux0XyE6iS0ESByK6UySbMZwi",
-      		oauth_token: oauth_token,
-      		oauth_token_secret: oauth_token_secret
-        	})
-				note_count = 0
+          consumer_key: "HkKQKdz8qmb2WTWTPWjd0FkRFQIGihbBkASyVdTtkIeihRFt4Z",
+          consumer_secret: "NScqGFcG2ymequqpFWMe7qKIs3ux0XyE6iS0ESByK6UySbMZwi",
+          oauth_token: oauth_token,
+          oauth_token_secret: oauth_token_secret
+          })
+        note_count = 0
         @@client.tagged(@@tag, :limit => 20).each do |p|
-					if p["note_count"] > note_count
-						note_count = p["note_count"]
-						@post = p
-					end
-				end
-				@@client.reblog @@client.info['user']['name']+".tumblr.com", id: @post['id'], reblog_key: @post['reblog_key']
-				@@globalSession = false
+          if p["note_count"] > note_count
+            note_count = p["note_count"]
+            @post = p
+          end
+        end
+        @@client.reblog @@client.info['user']['name']+".tumblr.com", id: @post['id'], reblog_key: @post['reblog_key']
+        @@globalSession = false
         render nothing: true   
   end
 
-	# def tokenGrabber
-	# 	byebug
-	# end
+  # def tokenGrabber
+  #   byebug
+  # end
 end
